2dc8522d6d3ff6e88743e99a632cabce06880f7b
Jason Yi <jasonyi1413@gmail.com>
2025-10-12 05:25:04 +1100

    feat: add friend pfps to remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.shiroko.me',
+        pathname: '/discord/avatar/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
