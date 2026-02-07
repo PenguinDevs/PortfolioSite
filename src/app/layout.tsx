fceef4962f76d14abcc5410742eb9589db332623
Jason Yi <jasonyi1413@gmail.com>
2026-02-07 18:10:20 +1100

    feat: prevent dark reader from modifying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="darkreader-lock" />
+      </head>
       <body>{children}</body>
     </html>
   );
