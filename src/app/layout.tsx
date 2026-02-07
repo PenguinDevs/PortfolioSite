8b0b46305198deaa8dfe1336d1169b037b62ac3d
Jason Yi <jasonyi1413@gmail.com>
2026-02-07 18:58:08 +1100

    feat: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+  openGraph: {
+    title: "Jason Yi (@penguindevs)",
+    description: "My portfolio site",
+    images: [{ url: '/og-image.jpg', width: 1200, height: 630 }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Jason Yi (@penguindevs)",
+    description: "My portfolio site",
+    images: ['/og-image.jpg'],
+  },
 };
 
 export default function RootLayout({
